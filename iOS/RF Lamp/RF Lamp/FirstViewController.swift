68fb76e7fde8a05901c1996bcc35f5dd0fec89b6
David A. Shamma <aymanshamma@gmail.com>
2015-02-08 00:09:31 -0800

    Pushing Swift App that might not stick around.

---
+++ iOS/RF Lamp/RF Lamp/FirstViewController.swift
@@ -0,0 +1,23 @@
+//
+//  FirstViewController.swift
+//  RF Lamp
+//
+//  Created by David A. Shamma on 2/7/15.
+//  GPL 3.0.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
---
+++ iOS/RF Lamp/RF Lamp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  RF Lamp
+//
+//  Created by David A. Shamma on 2/7/15.
+//  GPL 3.0.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

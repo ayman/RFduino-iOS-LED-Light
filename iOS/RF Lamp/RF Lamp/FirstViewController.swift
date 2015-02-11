5fca76dd5611fd266344c4ccedd4df22bf829dad
David A. Shamma <aymanshamma@gmail.com>
2015-02-10 21:32:33 -0800

    Adding more of the bluetooth stack.

--- iOS/RF Lamp/RF Lamp/FirstViewController.swift
+++ iOS/RF Lamp/RF Lamp/FirstViewController.swift
@@ -12,15 +12,16 @@ import CoreBluetooth
 
 // Example: https://gist.github.com/nolili/a583ea045dafafebb17f
 
-class FirstViewController: UIViewController, CBCentralManagerDelegate {
+class FirstViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate {
     var central: CBCentralManager!
-    
+    var rfduino: CBPeripheral!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         println("loaded")
-        // central = CBCentralManager(delegate: self, queue: dispatch_get_main_queue())
-        central = CBCentralManager(delegate: self, queue: nil)
+        central = CBCentralManager(delegate: self, queue: dispatch_get_main_queue())
+        // central = CBCentralManager(delegate: self, queue: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,14 +29,6 @@ class FirstViewController: UIViewController, CBCentralManagerDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    func centralManager(central: CBCentralManager!,
-                        didDiscoverPeripheral peripheral: CBPeripheral!,
-                        advertisementData: (NSDictionary), RSSI: NSNumber!) {
-        println("Discovered: \(peripheral.name)")
-        // self.connectingPeripheral = peripheral
-        // centralManager.stopScan()
-        // self.centralManager.connectPeripheral(peripheral, options: nil)
-    }
     
     func centralManagerDidUpdateState(central: CBCentralManager!) {
         var msg = ""
@@ -46,15 +39,56 @@ class FirstViewController: UIViewController, CBCentralManagerDelegate {
             msg = "CoreBluetooth BLE hardware is powered on and ready"
             self.central.scanForPeripheralsWithServices(nil, options: nil)
         case .Resetting:
-            msg = "CoreBluetooth BLE hardware is resetting"            
+            msg = "CoreBluetooth BLE hardware is resetting"
         case .Unauthorized:
-            msg = "CoreBluetooth BLE state is unauthorized"           
+            msg = "CoreBluetooth BLE state is unauthorized"
         case .Unknown:
-            msg = "CoreBluetooth BLE state is unknown"            
+            msg = "CoreBluetooth BLE state is unknown"
         case .Unsupported:
             msg = "CoreBluetooth BLE hardware is unsupported on this platform"
         }
         println("\(msg)")
+    }
+    
+    func centralManager(central: CBCentralManager!,
+                        didDiscoverPeripheral peripheral: CBPeripheral!,
+                        advertisementData: (NSDictionary), RSSI: NSNumber!) {
+        println("Discovered: \(peripheral.name)")
+        if (peripheral.name != nil && peripheral.name == "RFduino") {
+            self.central.stopScan()
+            self.rfduino = peripheral
+            self.rfduino.delegate = self
+            self.central.connectPeripheral(peripheral, options: nil)
+        }
+    }
+    
+    func centralManager(central: CBCentralManager!,
+                        didConnectPeripheral peripheral: CBPeripheral!) {
+        peripheral.discoverServices(nil)
+    }
+
+    func peripheral(peripheral: CBPeripheral!, didDiscoverServices error: NSError!) {
+        if let actualError = error {
+            // Handle Error
+        }
+        else {
+            for service in peripheral.services as [CBService]!{
+                println("Service: \(service.description)")
+                peripheral.discoverCharacteristics(nil, forService: service)
+            }
+        }
+    }
+    
+    func peripheral(peripheral: CBPeripheral!,
+                    didDiscoverCharacteristicsForService service: CBService!,
+                    error: NSError!) {
+        if let actualError = error{
+            
+        } else {
+            for characteristic in service.characteristics as [CBCharacteristic]{
+                println(" \(characteristic.UUID.UUIDString)")
+            }
+        }
     }
 
 }

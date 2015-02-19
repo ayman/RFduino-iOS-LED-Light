fca91f58d64bb92cf74652c2d2f731919e6f1f55
David A. Shamma <aymanshamma@gmail.com>
2015-02-18 23:40:01 -0800

    Moving RFduino controller to separate class. Removed HomeKit dependency.

--- iOS/RF Lamp/RF Lamp/FirstViewController.swift
+++ iOS/RF Lamp/RF Lamp/FirstViewController.swift
@@ -11,16 +11,8 @@ import CoreBluetooth
 
 // Example: https://gist.github.com/nolili/a583ea045dafafebb17f
 
-class FirstViewController:
-      UIViewController,
-      CBCentralManagerDelegate,
-      CBPeripheralDelegate {
-    var central: CBCentralManager!
-    var rfduino: CBPeripheral!
-    var service: CBService!
-    var sendChar: CBCharacteristic!
-    var receiveChar: CBCharacteristic!
-    var disconnnectChar: CBCharacteristic!
+class FirstViewController: UIViewController {
+    var rfduino : RFduino!
     
     @IBOutlet var onSwitch : UISwitch!
     @IBOutlet var onLabel : UILabel!
@@ -38,8 +30,9 @@ class FirstViewController:
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         println("loaded")
-        central = CBCentralManager(delegate: self, queue: dispatch_get_main_queue())
-        // central = CBCentralManager(delegate: self, queue: nil)
+        // central = CBCentralManager(delegate: self, queue: dispatch_get_main_queue())
+        rfduino = RFduino()
+        rfduino.startDiscovery()
     }
 
     override func didReceiveMemoryWarning() {
@@ -53,7 +46,7 @@ class FirstViewController:
             rawArray = [0x00, 0x00, 0x00]
         }
         let data = NSData(bytes: &rawArray, length: rawArray.count)
-        rfduino.writeValue(data, forCharacteristic: sendChar, type: CBCharacteristicWriteType.WithoutResponse)
+        rfduino.writeValue(data)
     }
     
     @IBAction func lightValue(sender: AnyObject) {
@@ -65,7 +58,7 @@ class FirstViewController:
             rawArray = [0x0A, 0x0A, 0x0A]
         }
         let data = NSData(bytes: &rawArray, length: rawArray.count)
-        rfduino.writeValue(data, forCharacteristic: sendChar, type: CBCharacteristicWriteType.WithoutResponse)
+        rfduino.writeValue(data)
     }
 
     @IBAction func sliderChanged(sender: AnyObject) {
@@ -96,79 +89,6 @@ class FirstViewController:
         var bd = UInt8(255 * bf)
         var rawArray:[UInt8] = [rd, gd, bd];
         let data = NSData(bytes: &rawArray, length: rawArray.count)
-        rfduino.writeValue(data, forCharacteristic: sendChar, type: CBCharacteristicWriteType.WithoutResponse)
-    }
-    
-    func centralManagerDidUpdateState(central: CBCentralManager!) {
-        var msg = ""
-        switch (central.state) {
-        case .PoweredOff:
-            msg = "CoreBluetooth BLE hardware is powered off"
-        case .PoweredOn:
-            msg = "CoreBluetooth BLE hardware is powered on and ready"
-            self.central.scanForPeripheralsWithServices(nil, options: nil)
-        case .Resetting:
-            msg = "CoreBluetooth BLE hardware is resetting"
-        case .Unauthorized:
-            msg = "CoreBluetooth BLE state is unauthorized"
-        case .Unknown:
-            msg = "CoreBluetooth BLE state is unknown"
-        case .Unsupported:
-            msg = "CoreBluetooth BLE hardware is unsupported on this platform"
-        }
-        println("\(msg)")
-    }
-    
-    func centralManager(central: CBCentralManager!,
-                        didDiscoverPeripheral peripheral: CBPeripheral!,
-                        advertisementData: (NSDictionary), RSSI: NSNumber!) {
-        println("Discovered: \(peripheral.name)")
-        if (peripheral.name != nil && peripheral.name == "RFduino") {
-            self.central.stopScan()
-            self.rfduino = peripheral
-            self.rfduino.delegate = self
-            self.central.connectPeripheral(peripheral, options: nil)
-        }
-    }
-    
-    func centralManager(central: CBCentralManager!,
-                        didConnectPeripheral peripheral: CBPeripheral!) {
-        peripheral.discoverServices(nil)
-    }
-
-    func peripheral(peripheral: CBPeripheral!, didDiscoverServices error: NSError!) {
-        if let actualError = error {
-            // Handle Error
-        }
-        else {
-            for service in peripheral.services as [CBService]!{
-                println("Service: \(service.description)")
-                self.service = service;
-                peripheral.discoverCharacteristics(nil, forService: service)
-            }
-        }
-    }
-    
-    func peripheral(peripheral: CBPeripheral!,
-                    didDiscoverCharacteristicsForService service: CBService!,
-                    error: NSError!) {
-        if let actualError = error{
-            println("There was an error...who knows....")            
-        } else {
-            for characteristic in service.characteristics as [CBCharacteristic]{
-                println(" \(characteristic.UUID.UUIDString)")
-                switch (characteristic.UUID.UUIDString) {
-                // These are hardcoded in the RFDuino
-                case "2221":
-                    self.receiveChar = characteristic
-                case "2222":
-                    self.sendChar = characteristic
-                case "2223":
-                    self.disconnnectChar = characteristic
-                default:
-                    println("nothing")
-                }
-            }
-        }
-    }
+        rfduino.writeValue(data)
+    }    
 }
